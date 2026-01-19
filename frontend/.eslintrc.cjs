647245d2bd5da12cffe3e9bdab145be80fc88790
Shanid Sajjatuz Islam <shanidsajjatuz@gmail.com>
2026-01-20 02:23:17 +0600

    feat: add CI/CD pipeline with staging environment

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -4,17 +4,16 @@ module.exports = {
   extends: [
     'eslint:recommended',
     '@typescript-eslint/recommended',
-    'eslint:recommended',
-    '@typescript-eslint/recommended',
-    'eslint-plugin-react-hooks/exhaustive-deps',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
   },
 }
